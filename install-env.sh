77b78eb0baf9def189c15f76225e23ebc12a25bd
pub <pub@pubs-MacBook-Pro.local>
2019-07-07 15:10:09 +0300

    added install-env

---
+++ install-env.sh
@@ -0,0 +1,6 @@
+
+echo "installing brew package manager"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "installing docker-compose package"
+brew install docker-compose
